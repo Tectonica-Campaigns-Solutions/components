b52b608b82001b4fc6c4ea63051d33a701e7f7d0
marcelsafont <marcel.safont1983@gmail.com>
2022-10-11 12:56:13 +0200

    add props narrative block

--- dist/components/NarrativeBlock.js
+++ dist/components/NarrativeBlock.js
@@ -9,8 +9,22 @@ var _react = _interopRequireDefault(require("react"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function NarrativeBlock() {
-  return /*#__PURE__*/_react.default.createElement("div", null, "narrativeblock");
+function NarrativeBlock(_ref) {
+  let {
+    title = null,
+    content = null,
+    col1class = null,
+    col2class = null
+  } = _ref;
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: "container"
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "row"
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: col1class ? col1class : ''
+  }, title && /*#__PURE__*/_react.default.createElement("h2", null, title), content && /*#__PURE__*/_react.default.createElement("p", null, content)), /*#__PURE__*/_react.default.createElement("div", {
+    className: col2class ? col2class : ''
+  }, "columna 2")));
 }
 
 var _default = NarrativeBlock;
--- src/lib/components/NarrativeBlock.jsx
+++ src/lib/components/NarrativeBlock.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
-function NarrativeBlock() {
+function NarrativeBlock({title=null, content=null, col1class=null, col2class=null }) {
   return (
-    <div>narrativeblock</div>
+    <div className="container">
+      <div className="row">
+        <div className={col1class ? col1class : ''}>
+          { title && (
+            <h2>{title}</h2>
+          )}
+          {
+            content && (
+              <p>{ content }</p>
+            )
+          }
+        </div>
+        <div className={col2class ? col2class : ''}>
+            columna 2
+        </div>
+      </div>
+    </div>
   )
 }
 
