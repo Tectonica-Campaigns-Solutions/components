a1720fab20ea470de94fc60f0ff49541d2d2d38f
marcelsafont <marcel.safont1983@gmail.com>
2022-11-04 11:48:08 +0100

    remove commonjs output

--- rollup.config.js
+++ rollup.config.js
@@ -7,8 +7,7 @@ export default [
   {
     input: './src/index.js',
     output: [
-      { file: 'dist/index.js', format: 'cjs'},
-      { file: 'dist/index.es.js', format: 'es', exports: 'named'}
+      { file: 'dist/index.js', format: 'es', exports: 'named'}
     ],
     plugins: [
       babel({
