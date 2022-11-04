6e24764e3f88b287f58b0f1a7e8eaf492f174a33
marcelsafont <marcel.safont1983@gmail.com>
2022-11-04 11:44:15 +0100

    export as commonjs and module and added terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,15 @@
 import babel from 'rollup-plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
+import{ terser } from 'rollup-plugin-terser';
 
 export default [
   {
     input: './src/index.js',
-    output: [{ file: 'dist/index.js', format: 'cjs'}],
+    output: [
+      { file: 'dist/index.js', format: 'cjs'},
+      { file: 'dist/index.es.js', format: 'es', exports: 'named'}
+    ],
     plugins: [
       babel({
         exclude: 'node_modules/**',
@@ -13,6 +17,7 @@ export default [
       }),
       external(),
       resolve(),
+      terser(),
     ]
   }
 ]
