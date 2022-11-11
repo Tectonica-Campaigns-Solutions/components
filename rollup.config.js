0f1b3fbf9adef0fa753d70849e4419995434b9ee
marcelsafont <marcel.safont1983@gmail.com>
2022-11-11 12:23:54 +0100

    move to cjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,21 @@
 import babel from 'rollup-plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import{ terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default [
   {
     input: './src/index.js',
     output: [
-      { file: 'dist/index.js', format: 'es', exports: 'named'}
+      { file: 'dist/index.js', format: 'cjs'}
     ],
     plugins: [
       babel(),
+      commonjs({
+        namedExports: {
+          'node_modules/react-slick/dist/react-slick-min.js': [ 'Slider' ]
+        }
+      }),
       peerDepsExternal(),
       nodeResolve(),
     ]
